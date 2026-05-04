0052bd3ec2e0b68e7c4cc1418e3e571c4d6e6d83
MD Tarek Chowdhury <shahriarashraf279@gmail.com>
2026-05-04 08:23:33 +0600

    Added frontend ArcadeDashboard

---
+++ resources/js/pages/ArcadeDashboard.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { Link } from "@inertiajs/vue3"
+
+const props = defineProps(['game'])
+console.log(props.game)
+
+</script>
+
+<template>
+    <main>
+        <header>
+            <Link href="/dashboard">Arcade Dashboard</Link>
+        </header>
+        <article>
+            <ul>
+                <li v-for="item in game" :key="item.id"">
+                    <strong> {{ item.title }} </strong> - {{ item.emulator }}
+                </li>
+            </ul>
+        </article>
+    </main>
+</template>
